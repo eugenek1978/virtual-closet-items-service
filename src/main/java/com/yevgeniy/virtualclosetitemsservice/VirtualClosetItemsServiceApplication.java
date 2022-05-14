202565341fccaa0f4bf332c0085d67928401de9e
Yevgeniy Kovaliov <yevgeniy.kovaliov.ads@gmail.com>
2022-05-14 20:07:09 +0300

    Initial commit

---
+++ src/main/java/com/yevgeniy/virtualclosetitemsservice/VirtualClosetItemsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yevgeniy.virtualclosetitemsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClosetItemsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClosetItemsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yevgeniy/virtualclosetitemsservice/VirtualClosetItemsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yevgeniy.virtualclosetitemsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClosetItemsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
